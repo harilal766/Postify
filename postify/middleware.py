b77584d6b11715a92151bf7dbd25be041c37fa66
harilal766 <harilalsunil2@gmail.com>
2025-07-29 17:13:26 +0530

    ..

--- postify/middleware.py
+++ postify/middleware.py
@@ -4,15 +4,16 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
+    "http://localhost",
+    "http://localhost:8080",
     "https://postify-ashy.vercel.app",
     "http://127.0.0.1:8000", 
 ]
-
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins = origins,
     allow_credentials = True,
-    allow_methods = ["*"],
+    allow_methods = ["GET"],
     allow_headers = ["*"],
 )
