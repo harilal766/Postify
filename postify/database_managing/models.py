d68511f2f93b55ad176a41cfff2850ba70870b4c
harilal766 <harilalsunil2@gmail.com>
2025-08-06 22:01:48 +0530

    scheduled order details function added to models.py

--- postify/database_managing/models.py
+++ postify/database_managing/models.py
@@ -1,7 +1,12 @@
 from .database import Base
 from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import Session
 
-from ..environment_variables import order_table
+from ..environment_variables import order_table,db_connection
+
+from sqlalchemy import create_engine,select
+engine = create_engine(db_connection)
+
 
 class Scheduled_Order(Base):
     __tablename__ = order_table
@@ -12,3 +17,18 @@ class Scheduled_Order(Base):
     Mobile = Column(String)
     Products = Column(String)
     Entry_Date = Column(String)
+    
+    @classmethod
+    def find_scheduled_order(cls,id:str):
+        try:
+            with Session(engine) as session:
+                orders = session.scalars(
+                    select(cls).where(
+                        (cls.Order_ID == id) | (cls.Mobile == id)
+                    )
+                ).all()
+                if orders:
+                    return orders[-1]
+        except Exception as e:
+            print(e)
+
