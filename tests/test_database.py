63329ff4b9a4f2a3e6a92502b2916032fb4bd136
harilal766 <harilalsunil2@gmail.com>
2025-08-08 13:21:09 +0530

    added test case for shopify

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,7 +1,5 @@
 import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from postify.database_managing.database import * 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) 
 import pytest
 from postify.environment_variables import *
 
