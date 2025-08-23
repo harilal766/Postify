af77820ffd8f0db9a213075672e855b11b8cabdd
harilal766 <harilalsunil2@gmail.com>
2025-08-23 13:22:34 +0530

    encapsulated order endpoints in a class

--- postify/main.py
+++ postify/main.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import Depends, FastAPI, HTTPException, Request, Form,UploadFile,File
+from fastapi import Depends, FastAPI, HTTPException, Request, Form,UploadFile,File, APIRouter
 from fastapi.security import OAuth2PasswordBearer
 
 from fastapi.responses import HTMLResponse, JSONResponse
@@ -20,8 +20,101 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 import requests
 import pytz
 
+class Order:
+    def __init__(self):
+        self.base_url = '/orders/'
+        
+    def get_order(self,identification : str):
+        order_response = {
+            "Name" : None,"Order_id" : None,
+            "Mobile" : None,"Status" : None
+        }
+        status = None; sh_inst = Shopify(order_id=identification)
+        try:
+            scheduled_order = Scheduled_Order().find_scheduled_order(id=identification)
+            if scheduled_order:
+                order_response["Name"] = scheduled_order.Name
+                order_response["Order_id"] = scheduled_order.Order_ID
+                order_response["Mobile"] = scheduled_order.Mobile
+                
+                order_response.update({
+                    "Speedpost Tracking Id" : scheduled_order.Barcode,
+                })
+                order_response.update({
+                    "Scheduled on " : scheduled_order.Entry_Date
+                })
+                # Order Status
+                if scheduled_order.is_bagged() == True:
+                    third_party = f"https://www.aftership.com/track/india-post/{scheduled_order.Barcode}"
+                    indiapost = "https://www.indiapost.gov.in"
+                    order_response['Status'] = f"Tracking : {third_party}"
+                elif scheduled_order.is_bagged() == False:
+                    order_response["Status"] = "Scheduled, Tracking link will be available soon."  
+            else:
+                unscheduled_order = sh_inst.search_in_all_stores()
+                status = 200
+                if unscheduled_order:
+                    unscheduled_order = unscheduled_order.get("node",None)
+                    customer = unscheduled_order.get("billingAddress",None)
+                    status = "Found in unscheduled orders"
+                    if customer:
+                        order_response["Name"] = customer["name"]
+                        order_response["Mobile"] = customer["phone"]
+                    order_response["Order_id"] = unscheduled_order["name"]
+                    order_response["Order date"] =  unscheduled_order["createdAt"].split("T")[0]
+                    order_response["Status"] = f"Order Confirmed, will Despatch on next working day."
+                else:
+                    status = 404
+            return order_response
+        except Exception as e:
+            print(f"Order detail error : {e}")
+            
+    def order_page(self, request : Request, identification : str):
+        order = None
+        try:
+            order = self.get_order(identification=identification)
+            if order:
+                status = 200
+                tracking_id_pattern = r'^EL\d{9}IN'
+                link_pattern = r'https://.*'
+                
+                for key,value in order.items():
+                    
+                    link_matches = re.search(link_pattern,value)
+                    tracking_id_matches = re.match(tracking_id_pattern,value)
+                    
+                    if link_matches:
+                        matched_link = link_matches.group()
+                        value = value.replace(
+                            matched_link, 
+                            f"<a target='_blank' href='{matched_link}'>{matched_link.strip("https://www.")}</a>"
+                        )
+                    
+                    if tracking_id_matches:
+                        matched_tracking_id = tracking_id_matches.group()
+                        tag = 'span'
+                        value = value.replace(
+                            matched_tracking_id, 
+                            f"<{tag} type='text' class='copy'>{matched_tracking_id}</{tag}>"
+                        )
+                    
+                    order[key] = value
+            else:
+                status = 404
+            return templates.TemplateResponse(request=request, name="tracking_result.html", context={"order" : order}) 
+            #return HTMLResponse(content=html_template,status_code=status)
+        except Exception as e:
+            print(f"Order page error : {e}")
+
+order = Order(); router = APIRouter()
+
+router.add_api_route(order.base_url + "{identification}",order.get_order,methods=["GET"])
+router.add_api_route(order.base_url + "{identification}/html",order.order_page,methods=["GET"])
 
 app = FastAPI()
+app.include_router(router)
+
+
 app.add_middleware(
     CORSMiddleware,allow_origins = ALLOWED_ORIGINS,allow_credentials = True,
     allow_methods = ["GET"],allow_headers = ["*"],
@@ -30,90 +123,8 @@ app.add_middleware(
 app.mount("/postify/static", StaticFiles(directory="postify/static"), name="postify_static")
 templates = Jinja2Templates(directory="postify/templates")
 
-@app.get("/orders/{identification}")
-def get_order(identification : str):
-    order_response = {
-        "Name" : None,"Order_id" : None,
-        "Mobile" : None,"Status" : None
-    }
-    status = None; sh_inst = Shopify(order_id=identification)
-    try:
-        scheduled_order = Scheduled_Order().find_scheduled_order(id=identification)
-        if scheduled_order:
-            order_response["Name"] = scheduled_order.Name
-            order_response["Order_id"] = scheduled_order.Order_ID
-            order_response["Mobile"] = scheduled_order.Mobile
-            
-            order_response.update({
-                "Speedpost Tracking Id" : scheduled_order.Barcode,
-            })
-            order_response.update({
-                "Scheduled on " : scheduled_order.Entry_Date
-            })
-            # Order Status
-            if scheduled_order.is_bagged() == True:
-                third_party = f"https://www.aftership.com/track/india-post/{scheduled_order.Barcode}"
-                indiapost = "https://www.indiapost.gov.in"
-                order_response['Status'] = f"Tracking : {third_party}"
-            elif scheduled_order.is_bagged() == False:
-                order_response["Status"] = "Scheduled, Tracking link will be available soon."  
-        else:
-            unscheduled_order = sh_inst.search_in_all_stores()
-            status = 200
-            if unscheduled_order:
-                unscheduled_order = unscheduled_order.get("node",None)
-                customer = unscheduled_order.get("billingAddress",None)
-                status = "Found in unscheduled orders"
-                if customer:
-                    order_response["Name"] = customer["name"]
-                    order_response["Mobile"] = customer["phone"]
-                order_response["Order_id"] = unscheduled_order["name"]
-                order_response["Order date"] =  unscheduled_order["createdAt"].split("T")[0]
-                order_response["Status"] = f"Order Confirmed, will Despatch on next working day."
-            else:
-                status = 404
-        return order_response
-    except Exception as e:
-        print(f"Order detail error : {e}")
 
-@app.get("/orders/{identification}/html",status_code = 200, response_class=HTMLResponse)
-def order_page(request : Request, identification : str):
-    order = None
-    try:
-        order = get_order(identification=identification)
-        if order:
-            status = 200
-            
-            tracking_id_pattern = r'^EL\d{9}IN'
-            link_pattern = r'https://.*'
-            
-            for key,value in order.items():
-                
-                link_matches = re.search(link_pattern,value)
-                tracking_id_matches = re.match(tracking_id_pattern,value)
-                
-                if link_matches:
-                    matched_link = link_matches.group()
-                    value = value.replace(
-                        matched_link, 
-                        f"<a target='_blank' href='{matched_link}'>{matched_link.strip("https://www.")}</a>"
-                    )
-                
-                if tracking_id_matches:
-                    matched_tracking_id = tracking_id_matches.group()
-                    tag = 'span'
-                    value = value.replace(
-                        matched_tracking_id, 
-                        f"<{tag} type='text' class='copy'>{matched_tracking_id}</{tag}>"
-                    )
-                
-                order[key] = value
-        else:
-            status = 404
-        return templates.TemplateResponse(request=request, name="tracking_result.html", context={"order" : order}) 
-        #return HTMLResponse(content=html_template,status_code=status)
-    except Exception as e:
-        print(f"Order page error : {e}")
+
         
 @app.get("/missing_form")
 def missing_form(request:Request):
@@ -128,7 +139,6 @@ def missing_form(request:Request):
     except Exception as e:
         print(e)
         
-
 import pandas as pd
 from io import StringIO
 
