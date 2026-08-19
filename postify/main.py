2beba3df6d2c9fac6ee3fda88c43ad6e37dcf44f
Harilal Sunil <harilalsunil2@gmail.com>
2026-08-19 11:04:57 +0530

    Update main.py

--- postify/main.py
+++ postify/main.py
@@ -139,9 +139,9 @@ class Tracking:
             if "https" in order["Status"]:
                 tracking_id = order.get("Speedpost Tracking Id",None)
                 aftership = f'https://www.aftership.com/track/india-post/{tracking_id}'
-                myspeedpost = f"https://myspeedpost.com/track?n={tracking_id}"
+                myspeedpost = f"https://myspeedpost.org/?track={tracking_id}"
                 if tracking_id:
-                    return RedirectResponse(url=aftership)
+                    return RedirectResponse(url=myspeedpost)
             else:
                 tracking_id_pattern = r'^EL\d{9}IN'
                 link_pattern = r'https://.*'
